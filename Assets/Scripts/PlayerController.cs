c6dc9dbc6486aed8a105666b3b7cabc02cc8b8c6
Mr-Triangle <dox.cactus@yandex.ru>
2025-10-30 21:30:41 +0500

    "Read keyboard input to control 2D character movement"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,9 +12,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f;
+        float vertical = 0.0f;
+
+        if (Keyboard.current.leftArrowKey.IsPressed() || Keyboard.current.aKey.IsPressed())
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.IsPressed())
+        {
+
+            horizontal = 1.0f;
+
+        }
+        else
+        {
+            horizontal = 0f;
+        }
+
+        if (Keyboard.current.leftArrowKey.IsPressed() || Keyboard.current.sKey.IsPressed())
+        {
+            vertical = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.wKey.IsPressed())
+        {
+
+            vertical = 1.0f;
+
+        }
+        else
+        {
+            vertical = 0f;
+        }
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        position.y = position.y + 0.1f;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
 }
