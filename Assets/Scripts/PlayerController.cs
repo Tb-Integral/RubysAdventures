0350d8e90440ef12271a161bd78b9a5c42ee7662
Mr-Triangle <dox.cactus@yandex.ru>
2025-10-31 09:06:50 +0500

    "Create Input Actions for player character movement"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,51 +3,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        float vertical = 0.0f;
-
-        if (Keyboard.current.leftArrowKey.IsPressed() || Keyboard.current.aKey.IsPressed())
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.IsPressed())
-        {
-
-            horizontal = 1.0f;
-
-        }
-        else
-        {
-            horizontal = 0f;
-        }
-
-        if (Keyboard.current.leftArrowKey.IsPressed() || Keyboard.current.sKey.IsPressed())
-        {
-            vertical = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.wKey.IsPressed())
-        {
-
-            vertical = 1.0f;
-
-        }
-        else
-        {
-            vertical = 0f;
-        }
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Vector2 position = (Vector2)transform.position + 0.1f * move;
         transform.position = position;
     }
 }
