81353d690b2d1140d46be12917fa84ddc81c07be
Mr-Triangle <dox.cactus@yandex.ru>
2025-11-02 23:22:27 +0500

    "Implement object collisions for your 2D game"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,25 @@ public class PlayerController : MonoBehaviour
     public float speed = 3f;
     public InputAction MoveAction;
 
+    private Rigidbody2D rb;
+    private Vector2 move;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + speed * move * Time.deltaTime;
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rb.position + speed * move * Time.deltaTime;
+        rb.MovePosition(position);
     }
 }
