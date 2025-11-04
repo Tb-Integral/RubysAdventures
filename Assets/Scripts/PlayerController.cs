b27f8fe9e71c5bdfc99308cfe7ab796955136ef8
Mr-Triangle <dox.cactus@yandex.ru>
2025-11-04 16:21:40 +0500

    "Set up a basic health system"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,18 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 3f;
     public InputAction MoveAction;
+    public int maxHealth = 5;
 
     private Rigidbody2D rb;
     private Vector2 move;
+    private int currentHealth;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
         rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -26,5 +29,10 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 position = (Vector2)rb.position + speed * move * Time.deltaTime;
         rb.MovePosition(position);
+    }
+
+    private void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
     }
 }
